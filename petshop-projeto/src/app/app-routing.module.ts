407beca58f7badf9c2eefa125aac83215c132557
TheuZCoder <matheusrdsilva1@gmail.com>
2023-11-09 22:50:12 -0300

    atualização footer

--- petshop-projeto/src/app/app-routing.module.ts
+++ petshop-projeto/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FirstPageComponent } from './components/first-page/first-page.component';
+
 import { LoginCadastroComponent } from './components/login-cadastro/login-cadastro.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: FirstPageComponent
-  },
   {
     path: 'login-cadastro',
     component: LoginCadastroComponent
