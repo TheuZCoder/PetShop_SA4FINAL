25d5bbcdd8d3044752e07480ba9ba6ebdda1c7ea
TheuZCoder <matheusrdsilva1@gmail.com>
2023-12-12 10:32:42 -0300

    atualizado TS login logout

--- petshop-projeto/src/app/service/cliente.service.ts
+++ petshop-projeto/src/app/service/cliente.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map} from 'rxjs/operators';
 import { Cliente } from '../modelo/cliente.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
+  constructor(private http: HttpClient) {}
   private apiUrl = 'http://localhost:3000/clientes';
   public isLoggedIn = false; // Torna a propriedade pública
 
@@ -15,26 +16,9 @@ export class ClienteService {
     return this.isLoggedIn;
   }
 
-  constructor(private http: HttpClient) {}
 
   cadastrarCliente(cliente: any): Observable<any> {
     return this.http.post(this.apiUrl, cliente);
-  }
-
-  fazerLogin(email: string, senha: string): Observable<boolean> {
-    return this.http.get<Cliente[]>(`${this.apiUrl}?email=${email}&senha=${senha}`).pipe(
-      map((users: Cliente[]) => {
-        const user = users[0]; // Supondo que apenas um usuário corresponderá ao login
-        if (user) {
-          this.isLoggedIn = true;
-        }
-        return !!user; // Retorna true se o usuário for encontrado, false caso contrário
-      }),
-      catchError(error => {
-        this.isLoggedIn = false;
-        return of(false);
-      })
-    );
   }
 
   getUsers(): Observable<Cliente[]> {
